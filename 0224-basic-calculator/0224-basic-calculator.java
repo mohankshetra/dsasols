11f37f11ab1cc6e0eabf0c46b83b16de731492ff
Kshetra mohan Atty <kshetramohanatty@gmail.com>
2026-07-20 23:12:25 +0530

    Time: 10 ms (80.92%), Space: 46.5 MB (42.2%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int calculate(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                number = number * 10 + (ch - '0');
+            }
+
+            else if (ch == '+') {
+                result += sign * number;
+                sign = 1;
+                number = 0;
+            }
+
+            else if (ch == '-') {
+                result += sign * number;
+                sign = -1;
+                number = 0;
+            }
+
+            else if (ch == '(') {
+
+                stack.push(result);
+                stack.push(sign);
+
+                result = 0;
+                sign = 1;
+            }
+
+            else if (ch == ')') {
+
+                result += sign * number;
+
+                number = 0;
+
+                result *= stack.pop();      // sign
+
+                result += stack.pop();      // previous result
+            }
+        }
+
+        return result + sign * number;
+    }
+}
