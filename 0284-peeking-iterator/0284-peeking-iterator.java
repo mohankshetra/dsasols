98a09fc984bd53e2b178b3a13732081516790423
Kshetra mohan Atty <kshetramohanatty@gmail.com>
2026-07-24 11:58:30 +0530

    Time: 4 ms (81.77%), Space: 43.9 MB (9.36%) - LeetHub

---
+++ 0284-peeking-iterator/0284-peeking-iterator.java
@@ -0,0 +1,35 @@
+class PeekingIterator implements Iterator<Integer> {
+
+    private Iterator<Integer> iterator;
+    private Integer nextElement;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+
+        this.iterator = iterator;
+
+        if (iterator.hasNext())
+            nextElement = iterator.next();
+    }
+
+    public Integer peek() {
+        return nextElement;
+    }
+
+    @Override
+    public Integer next() {
+
+        Integer current = nextElement;
+
+        if (iterator.hasNext())
+            nextElement = iterator.next();
+        else
+            nextElement = null;
+
+        return current;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextElement != null;
+    }
+}
